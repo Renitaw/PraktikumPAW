f9f6a6641118739fa9b689124f6988eae47b2a33
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-08-04 10:44:36 +0700

    Menambahkan method For Each

---
+++ src/main/java/com/helloworld/helloworld/ForEach.java
@@ -0,0 +1,12 @@
+package com.helloworld.helloworld;
+
+public class ForEach {
+    public static void main(String[] args){
+        String[] array ={
+            "Renita", "Widiastuti", "Sayur", "Sayur"
+        };
+        for (var value : array) {
+            System.out.println(value);
+        }
+    }
+}
