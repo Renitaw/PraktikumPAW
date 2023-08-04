edf81254a62a6ab82dcdbdf8e082de3efce61945
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-08-04 10:38:18 +0700

    Menambahkan method tanpa for each

---
+++ src/main/java/com/helloworld/helloworld/TanpaForEach.java
@@ -0,0 +1,12 @@
+package com.helloworld.helloworld;
+
+public class TanpaForEach {
+    public static void main(String[] args){
+        String[] array = {
+            "Renita", "Widiastuti", "Sayur", "Sayur"
+        };
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
