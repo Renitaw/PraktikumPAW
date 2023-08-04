13b78f960b4abf18735e05f5f5192d5876f03601
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-08-04 10:50:43 +0700

    Menambahkan fungsi for method

---
+++ src/main/java/com/helloworld/helloworld/Method.java
@@ -0,0 +1,11 @@
+package com.helloworld.helloworld;
+
+public class Method {
+    public static void main(String[] args){
+        sayHelloWold();  
+    }
+
+    static void sayHelloWold(){
+        System.out.println("Hello World");
+    }
+}
