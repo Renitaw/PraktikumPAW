6d70f93d1c71628cfc3f2df4186e6e2b1f409da0
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-08-04 10:11:08 +0700

    Menambahkan method perulangan post statement

---
+++ src/main/java/com/helloworld/helloworld/ForLoop_PostStatement.java
@@ -0,0 +1,11 @@
+package com.helloworld.helloworld;
+
+public class ForLoop_PostStatement {
+
+    public static void main(String[] args){
+        for (var counter = 1; counter <= 10; counter++) {
+        System.out.println("Perulangan ke-" + counter);
+        }
+    }
+
+}
