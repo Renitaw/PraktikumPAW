bcd45b8daf269ef922e2ccd39da46cef35b44cc9
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-08-04 10:27:43 +0700

    Menambahkan method WhileLoop

---
+++ src/main/java/com/helloworld/helloworld/WhileLoop.java
@@ -0,0 +1,15 @@
+package com.helloworld.helloworld;
+
+public class WhileLoop {
+    public static void main(String[] args){
+        var counter = 1;
+        
+        while (counter <= 10){
+            System.out.println("Perulangan ke-" + counter);
+            counter++;
+        }
+
+    }
+}
+    
+
