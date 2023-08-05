6526c001da59fe311937b345d336eb125bc63abe
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-08-05 10:03:44 +0700

    Menambahkan method Overloading

---
+++ src/main/java/com/helloworld/helloworld/MethodOverloading.java
@@ -0,0 +1,21 @@
+package com.helloworld.helloworld;
+
+public class MethodOverloading {
+public static void main(String[] args) {
+    sayHello();
+    sayHello("Renita");
+    sayHello("Renita", "Widiastuti");
+}
+
+    static void sayHello() {
+        System.out.println("Hello");
+    }
+
+    static void sayHello(String firstname) {
+        System.out.println("Hello" + firstname);
+    }
+
+    static void sayHello(String firstname, String lastname) {
+        System.out.println("Hello" + firstname + " " + lastname);
+    }
+}
