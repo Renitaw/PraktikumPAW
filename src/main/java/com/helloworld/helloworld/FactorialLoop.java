967719cb0fea7a2679521e79338eb13eac9c77c6
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-08-05 10:20:19 +0700

    Menambahkan method Factorial Recurcive

--- src/main/java/com/helloworld/helloworld/FactorialLoop.java
+++ src/main/java/com/helloworld/helloworld/FactorialLoop.java
@@ -1,7 +1,10 @@
 package com.helloworld.helloworld;
 
 public class FactorialLoop {
-    
+    public static void main(String[] args) {
+        int result = factorial(7);
+        System.out.println("Factorial: " + result);
+    }
         static int factorial(int value) {
             var result = 1;
             for (int i = 1; i <= value; i++){
---
+++ src/main/java/com/helloworld/helloworld/FactorialRecurcive.java
@@ -0,0 +1,16 @@
+package com.helloworld.helloworld;
+
+public class FactorialRecurcive {
+    public static void main(String[] args) {
+        int result = factorialRecursive(7);
+        System.out.println("Factorial: " + result);
+    }
+
+    static int factorialRecursive(int value) {
+        if (value == 1) {
+            return 1;
+        } else {
+            return value * factorialRecursive(value - 1);
+        }
+    }
+}
