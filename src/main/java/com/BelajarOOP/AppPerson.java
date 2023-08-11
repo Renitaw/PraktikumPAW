2baeee726161aee7554ac96132c744d13e8de24b
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-08-11 11:10:43 +0700

    Menambahkan code pada method person dan AppPerson

--- src/main/java/com/BelajarOOP/AppPerson.java
+++ src/main/java/com/BelajarOOP/AppPerson.java
@@ -8,12 +8,14 @@ public class AppPerson {
         // AppPerson person3;
         // person3 = new AppPerson();
 
-        person.name = "Renita";
+        person.name = " Renita";
         person.address = "TNG";
 
         System.out.println(person.name);
         System.out.println(person.address);
         System.out.println(person.country);
+
+        person.sayHello(" Teman-teman");
     }
 
 }
--- src/main/java/com/BelajarOOP/Person.java
+++ src/main/java/com/BelajarOOP/Person.java
@@ -4,4 +4,8 @@ public class Person {
     String name;
     String address;
     final String country = "Indonesia";
+
+    void sayHello(String paraName){
+        System.out.println("Selamat Pagi" + paraName + ", Nama Saya" + name);
+    }
 }
